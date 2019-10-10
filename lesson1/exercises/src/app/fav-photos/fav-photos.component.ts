fa90900253c23316b33d9e15c83cf92d799e8362
Melanie <mejr5b@gmail.com>
2019-10-10 10:11:24 -0500

    added photos and changed list

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,9 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'Mellys Photos';
   image1 = 'https://www.launchcode.org/assets/icons/trophy-95e8cbe9bfda44123422302951deb1c92a237d39052669b8fbfafec00cb4f608.png';
-  image2 = '';
+  image2 = 'https://i.pinimg.com/originals/2e/29/a1/2e29a1101f316a0ca12d48540a70248f.jpg';
   image3 = '';
 
   constructor() { }
