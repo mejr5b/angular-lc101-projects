1083709e1de6edbeeb93ea15df9600b470686366
Melanie <mejr5b@gmail.com>
2019-10-23 22:02:08 -0500

    complete exercise 3

--- lesson2/exercises/src/app/candidates/candidates.component.ts
+++ lesson2/exercises/src/app/candidates/candidates.component.ts
@@ -6,14 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./candidates.component.css']
 })
 export class CandidatesComponent implements OnInit {
-   missionName = "LaunchCode Moonshot"
-
+   missionName = "LaunchCode Moonshot";
+   editMissionName = false;
+   
    candidates = [
-    {name: 'Rusty Rutabaga', data: {age: 5, mass: '0.75 kg', sidekick: 'Blake'}, image: 'assets/images/Blake.png'},
-    {name: 'Tessa Tortoise', data: {age: 126, mass: '113 kg', sidekick: 'Sally'}, image: 'assets/images/Sally.png'},
-    {name: 'Bernie Beagle', data: {age: 4, mass: '15 kg', sidekick: 'Paul'}, image: 'assets/images/Paul.png'},
+    {name: 'Nicholas Jones', data: {age: 26, mass: '175 kg', sidekick: 'Blake'}, image: 'assets/images/Blake.png'},
+    {name: 'Melanie Jones', data: {age: 24, mass: '113 kg', sidekick: 'Sally'}, image: 'assets/images/Sally.png'},
+    {name: 'Warren Jones', data: {age: 4, mass: '15 kg', sidekick: 'Paul'}, image: 'assets/images/Paul.png'},
     {name: 'Jackalope', data: {age: 45, mass: '33 kg', sidekick: 'Chris'}, image: 'assets/images/Chris.png'},
-    {name: 'Champ', data: {age: 9, mass: '36 kg', sidekick: 'Carly'}, image: 'assets/images/Carly.png'}
+    {name: 'Mary Jones', data: {age: 54, mass: '36 kg', sidekick: 'Carly'}, image: 'assets/images/Carly.png'}
   ];
 
   crew = [];
@@ -22,11 +23,14 @@ export class CandidatesComponent implements OnInit {
 
   ngOnInit() {
   }
-
-  // Code the addToCrew function here:
-
-
-  // BONUS: Code the changeMissionName function here:
-
+  addToCrew(person: object) {
+    if(!this.crew.includes(person)){
+      this.crew.push(person);
+    }
+  }
+  changeMissionName(name: string){
+    this.missionName = name;
+    return false;
+ }
 
 }
